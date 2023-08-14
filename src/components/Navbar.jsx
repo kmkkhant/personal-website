feaa80a1121794b18a900b3fbc0a0d4464b1bde6
kaung-min-khant <1kaungminkhant1@gmail.com>
2023-08-14 12:22:31 -0700

    update nav bar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
           <img
             src={logotext}
             alt="logo"
-            className="sm:w-[90px] sm:h-[90px] w-[85px] h-[85px] -ml-[0.6rem] object-contain"
+            className="sm:w-[90px] sm:h-[90px] md:w-[180px] h-[85px] -ml-[0.6rem] object-contain"
           />
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-14 mt-2">
