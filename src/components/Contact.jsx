d2a62b4e77cbfd95ed85812221df99ec0ed6c676
kaung-min-khant <1kaungminkhant1@gmail.com>
2023-08-15 12:22:29 -0700

    update contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,16 +29,16 @@ const Contact = () => {
     //click on create a new template then click on save.
     emailjs
       .send(
-        'serviceID', // paste your ServiceID here (you'll get one when your service is created).
-        'templateID', // paste your TemplateID here (you'll find it under email templates).
+        'service_6qu4enq', // paste your ServiceID here (you'll get one when your service is created).
+        'template_qlv8obf', // paste your TemplateID here (you'll find it under email templates).
         {
           from_name: form.name,
-          to_name: 'YourName', // put your name here.
+          to_name: 'Kaung Min Khant', // put your name here.
           from_email: form.email,
-          to_email: 'youremail@gmail.com', //put your email here.
+          to_email: '1kaungminkhant1@gmail.com', //put your email here.
           message: form.message,
         },
-        'yourpublickey' //paste your Public Key here. You'll get it in your profile section.
+        'hOEnc_2OBKJmHLVPS' //paste your Public Key here. You'll get it in your profile section.
       )
       .then(
         () => {
